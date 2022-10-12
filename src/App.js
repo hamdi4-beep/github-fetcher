151e335a6f65d727a90fadd3245d285833cd0c33
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-10-12 16:16:15 -0700

    improvised code!

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React from 'react'
 import { User } from './User'
 import './App.css'
 
+function uploadImg(addImg) {
+    const input = document.createElement('input')
+
+    input.type = 'file'
+    input.click()
+
+    input.onchange = e => {
+        if (input.files[0]) {
+            const file = input.files[0]
+            const type = file.type
+
+            if (type.split('/')[0] !== 'image') return
+
+            addImg(URL.createObjectURL(file))
+        }
+    }
+
+    input.remove()
+}
+
 export default class App extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            user: null
+            user: {}
         }
     }
 
@@ -16,13 +36,37 @@ export default class App extends React.Component {
 
         return (
             <div className='wrapper'>
-                {user && (<User user={user} />)}
+                {user && (
+                    <User user={user}>
+                        <div className='user-img' onClick={e => this.handleClick(e)}>
+                            <img src={user.avatar_url} alt='' />
+                        </div>
+                    </User>
+                )}
             </div>
         )
     }
 
+    handleClick(e) {
+        const userImg = e.currentTarget
+
+        uploadImg(URL => {
+            this.setState(prevState => ({
+                user: {
+                    ...prevState.user,
+                    avatar_url: URL
+                }
+            }))
+
+            if (!userImg.classList.contains('pause')) userImg.classList.add('pause')
+        })
+
+        if (!userImg.classList.contains('spin')) userImg.classList.add('spin')
+        if (userImg.classList.contains('pause')) userImg.classList.remove('pause')
+    }
+
     componentDidMount() {
-        fetch('http://api.github.com/users/hamdi4-beep')
+        fetch('https://api.github.com/users/hamdi4-beep')
         .then(res => res.ok && res.json())
         .then(user => this.setState({ user }))
     }
--- src/User.js
+++ src/User.js
@@ -1,56 +1,16 @@
 export function User(props) {
     const user = props.user
 
-    const handleClick = e => {
-        const userImg = document.querySelector('.user-img')
-        
-        if (!userImg.classList.contains('spin')) userImg.classList.add('spin')
-        if (userImg.classList.contains('pause')) userImg.classList.remove('pause')
-
-        uploadImg(url => {
-            const img = userImg.querySelector('img')
-
-            img.src = url
-            img.onload = function() {
-                userImg.classList.add('pause')
-            }
-        })
-    }
-
     return (
         <div className='user-wrapper'>
             <div className='user-profile'>
-                <div className='user-img'>
-                    <img src={user.avatar_url} alt='' />
-                </div>
+                {props.children}
 
                 <div className='user-info'>
                     <h3>{user.name}</h3>
                     <p>@{user.login}</p>
                 </div>
             </div>
-
-            <button onClick={handleClick}>Change Profile Picture</button>
         </div>
     )
-}
-
-function uploadImg(addImg) {
-    const input = document.createElement('input')
-    input.type = 'file'
-    input.click()
-
-    input.onchange = function(e) {
-        if (this.files[0]) {
-            const file = this.files[0]
-            const type = file.type
-            const url = URL.createObjectURL(file)
-
-            if (type.split('/')[0] !== 'image') return
-
-            addImg(url)
-        }
-    }
-
-    input.remove()
 }
