90cd1036185e480a6da675961c3a25a101a8ec16
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2022-10-20 17:05:35 -0700

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import { UsersContainer } from './UsersContainer'
+import { useState } from 'react'
 import './styles'
 
-export default function App() {
+function Form({ onSubmit }) {
     return (
-        <UsersContainer />
+        <form onSubmit={onSubmit}>
+            <input type='text' name='username' placeholder='Aa' />
+            <button>Add User</button>
+        </form>
+    )
+}
+
+function User({ user, deleteUser }) {
+    return (
+        <div className='user__profile'>
+            <div className='user__img'>
+                <img src={user.avatar_url} alt='' />
+            </div>
+
+            <div className='user__info'>
+                <h3>{user.name}</h3>
+                <p>@{user.login}</p>
+                <button onClick={deleteUser}>Delete User</button>
+            </div>
+        </div>
+    )
+}
+
+export default function App() {
+    const [users, setUsers] = useState([])
+    const [query, setQuery] = useState('')
+
+    const handleSubmit = e => {
+        const formData = new FormData(e.currentTarget)
+        const username = formData.get('username')
+
+        if (!username) return
+
+        fetch(`https://api.github.com/users/${username}`)
+        .then(res => res.ok && res.json())
+        .then(user => setUsers(oldUsers => [...oldUsers, user]))
+
+        e.preventDefault()
+    }
+
+    const handleClick = target => setUsers(users.filter(user => target !== user))
+
+    const filteredUsers = users.filter(user => user.name.toLowerCase().includes(query.toLowerCase()))
+
+    return (
+        <div className='wrapper flex'>
+            <div>
+                <input type='search' onChange={({ currentTarget }) => setQuery(currentTarget.value)} placeholder='Search Users' />
+                
+                <div className='users__container'>
+                    {filteredUsers.map((user, i) => (
+                        <User key={i} user={user} deleteUser={() => handleClick(user)} />
+                    ))}
+                </div>
+            </div>
+
+            <Form onSubmit={handleSubmit} />
+        </div>
     )
 }
